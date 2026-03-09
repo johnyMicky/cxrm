eccd499525c021e26d69de607fbf63a54ff6e9d9
johnyMicky <166741409+johnyMicky@users.noreply.github.com>
2026-03-09 18:21:04 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "cxrm",
+  description: "Minimal CRM"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}

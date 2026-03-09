6226111e7663c20f0944df1ecc96349b1270e0e8
johnyMicky <166741409+johnyMicky@users.noreply.github.com>
2026-03-09 18:22:37 +0400

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,1 +1,16 @@
-
+export default function LoginPage() {
+  return (
+    <main className="page">
+      <div className="card">
+        <h1>Login</h1>
+        <input className="input" type="email" placeholder="Email" />
+        <input className="input" type="password" placeholder="Password" />
+        <div>
+          <button className="btn" type="button">
+            Sign In
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
